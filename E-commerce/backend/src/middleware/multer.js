25209d2451f83ed73843bc68666a583cacc0b100
Shivamkr025 <shivam22@navgurukul.org>
2024-05-25 22:31:14 +0530

    create multer middleware

---
+++ E-commerce/backend/src/middleware/multer.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../Assets')); 
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        const filename = file.fieldname + '-' + uniqueSuffix + ext;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = { upload };
