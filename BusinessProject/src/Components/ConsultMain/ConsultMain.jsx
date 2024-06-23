9f8f4efb05eab8b1693ac5c0684b93ba7bac210b
Shivamkr025 <shivam22@navgurukul.org>
2024-06-23 15:04:58 +0530

    update ConsultMain componet

--- BusinessProject/src/Components/ConsultMain/ConsultMain.jsx
+++ BusinessProject/src/Components/ConsultMain/ConsultMain.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
-import consult_Img from '../../assets/quote.jpg'
+import consult_Img from '../../assets/quote.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ConsultMain() {
     return (
-        <>
-            <div className="d-flex">
-                <div className="flex-fill h-100 p-5 bg-body-tertiary border mr-3 mt-4">
-                    <h2 className='mt-4'>Add borders</h2>
-                    <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
-                    <div className="mb-3 mt-4 d-flex">
-                        <input type="text" className="form-control me-2" placeholder="Full Name" style={{height:'3rem', width:'20rem'}} />
-                        <input type="email" className="form-control" placeholder="Email Address" style={{height:'3rem', width:'20rem'}} />
-                    </div>
-                    <div className="d-flex align-items-center mt-4">
-                        <select className="form-control me-2" style={{width:'20rem' , height:'3rem'}}>
-                            <option>Select A Service</option>
-                            <option>Option 2</option>
-                        </select>
-                        <button type="button" class="btn btn-danger btn-lg" style={{width:'20rem'}}>Large button</button>
+        <div className="p-4">
+            <div className="row">
+                <div className="col-lg-6 col-md-12 mb-4">
+                    <div className="h-100 p-5 bg-body-tertiary border">
+                        <h2 className='mt-4'>Add borders</h2>
+                        <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+                        <div className="mb-3 mt-4 d-flex flex-column flex-md-row">
+                            <input type="text" className="form-control me-md-2 mb-2 mb-md-0" placeholder="Full Name" style={{ height: '3rem', width: '100%' }} />
+                            <input type="email" className="form-control" placeholder="Email Address" style={{ height: '3rem', width: '100%' }} />
+                        </div>
+                        <div className="d-flex flex-column flex-md-row align-items-center mt-4">
+                            <select className="form-control me-md-2 mb-2 mb-md-0" style={{ height: '3rem' }}>
+                                <option>Select A Service</option>
+                                <option>Option 2</option>
+                            </select>
+                            <button type="button" className="btn btn-danger btn-lg" style={{ height: '3rem', width: '100%' }}>Large button</button>
+                        </div>
                     </div>
                 </div>
-                <div className="flex-fill h-100 bg-body-tertiary border rounded-3 mt-4">
-                    <img src={consult_Img} alt="" style={{height:'25rem', width:'45rem' , backgroundSize:'cover'}}/>
+                <div className="col-lg-6 col-md-12 mb-4">
+                    <div className="h-100 bg-body-tertiary border rounded-3">
+                        <img src={consult_Img} alt="" className="img-fluid" style={{ height: '25rem', objectFit: 'cover', width: '100%' }} />
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
